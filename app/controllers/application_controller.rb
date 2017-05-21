3bd422988c64b03e626feeb8c24e1ceb4ac7d34d
Eddie Kao <eddie@digik.com.tw>
2017-05-21 13:42:50 +0800

    add init_cart in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :init_cart
+
+  private
+  def init_cart
+    @cart = Cart.from_hash(session[:cart9487])
+  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,8 @@
 class CartsController < ApplicationController
 
   def add
-    cart = Cart.from_hash(session[:cart9487])
-    cart.add_item(params[:id])
-
-    session[:cart9487] = cart.to_hash
-
+    @cart.add_item(params[:id])
+    session[:cart9487] = @cart.to_hash
     redirect_to products_path, notice: "已放入購物車"
   end
 
