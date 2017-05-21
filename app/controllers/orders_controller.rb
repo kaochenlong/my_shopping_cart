1665bd6e3f71fc71f342196e318a64f1767b003b
Eddie Kao <eddie@digik.com.tw>
2017-05-21 15:53:08 +0800

    finish partial order feature

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  def create
+    @order = Order.new(order_params)
+
+    @cart.items.each do |item|
+      @order.order_items.build(product: item.product,
+                               price: item.product.price,
+                               quantity: item.quantity)
+    end
+
+    if @order.save
+      # 1. 付錢
+      #
+      # 2. 清空購物車
+      session[:cart9487] = nil
+
+      # 3. 走回 product 頁面
+      redirect_to products_path, notice: "感謝大爺!"
+    else
+      render "carts/checkout"
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:recipient, :tel, :email)
+  end
+end
