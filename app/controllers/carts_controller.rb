d213fa866bca5a2c2cbbe5137ce1ed11df49a512
Eddie Kao <eddie@digik.com.tw>
2017-05-21 14:29:35 +0800

    update cart show action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,12 @@
 class CartsController < ApplicationController
-
   def add
     @cart.add_item(params[:id])
     session[:cart9487] = @cart.to_hash
     redirect_to products_path, notice: "已放入購物車"
   end
 
+  def destroy
+    session[:cart9487] = nil
+    redirect_to products_path, notice: "購物車已清除!"
+  end
 end
